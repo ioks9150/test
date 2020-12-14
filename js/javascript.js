0d12a497520078ae2349031b833b48da58f8c948
ioks9150 <ioks9150@naver.com>
2020-12-15 00:24:48 +0900

    ddd

--- js/javascript.js
+++ js/javascript.js
@@ -12,34 +12,77 @@ $(document).ready(function(){
 
     
     
+    $('.buttonbox>a').hover(function () {
+            $(this).find('img').attr('src','images/arrow_r_h.png');
+        },
+        function () {
+            $(this).find('img').attr('src','images/arrow_r.png');
+        });
     
-    $('.buttonbox>a').mouseover(function(){
-        $(this).find('img').attr('src','images/arrow_r_h.png');
-    });
-    $('.buttonbox>a').mouseleave(function(){
-        $(this).find('img').attr('src','images/arrow_r.png');
-    });
+    
+    
     
     
     
     
 
 
-   $('.post-wrapper').slick({
+   $('.post-slider').slick({
       slidesToShow: 3,
       slidesToScroll: 1,
       autoplay: true,
       autoplaySpeed: 2000,
       nextArrow:$('.next'),
       prevArrow:$('.prev'),
+//       css({
+//           width : '32%'
+//       });
     });
     
     
-    $('.mainmenu>li>a').mouseover(function(){
-        $(this).find('.submenu').css({
-            'display' : 'block',
+
+    
+
+    
+        $('.mainmenu>li').on('mouseover',function(){
+            $(this).find('.submenu').stop().css({
+                display : 'block'
+            });
+            $(this).children('a').addClass('on');
+        });/*클래스gnb의 바로 밑의 li의 자식요소중 클래스sub에게 slidedown */
+        
+
+        
+        $('.mainmenu>li').on('mouseout',function(){
+            $(this).find('.submenu').stop().css({
+                display : 'none'
+            });
+            $(this).children('a').removeClass('on');
         });
-    });
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
